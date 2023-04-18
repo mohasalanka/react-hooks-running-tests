09e9c1174a5ac1c1e7dd87ce7701e133908f24b1
Mohamed <mohasalanka2020@gmail.com>
2023-04-18 14:49:50 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import Article from './Article';
+import Header from './Header';
 
 function App() {
   return <div className="App">
